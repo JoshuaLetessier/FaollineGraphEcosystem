2b7ba03e48e33fd1dc59f021b4ec274d43ad0bfb
JoshuaLetessier <147531448+JoshuaLetessier@users.noreply.github.com>
2026-06-01 16:59:05 +0200

    fix(graphdialoguesystem): add missing using for GraphLocalizationAdapterRegistry

--- com.faolline.graphdialoguesystem/Editor/Localization/DialogueGraphLocalizationAdapter.cs
+++ com.faolline.graphdialoguesystem/Editor/Localization/DialogueGraphLocalizationAdapter.cs
@@ -3,6 +3,7 @@ using UnityEditor;
 using UnityEngine;
 using Faolline.GraphCore;
 using Faolline.GraphLocalization;
+using Faolline.GraphLocalization.Editor;
 
 namespace Faolline.GraphDialogue.Editor
 {
